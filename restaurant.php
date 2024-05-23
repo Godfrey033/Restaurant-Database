f7cebaf31e2cbccfc7081bfc2a3efbc55c38f218
Godfrey033 <157035448+Godfrey033@users.noreply.github.com>
2024-05-23 21:08:46 +0300

    Add files via upload

---
+++ restaurant.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `restaurant`
+ */
+
+/* `restaurant`.`customers` */
+$customers = array(
+  array('CustomerID' => '1','FirstName' => '[Godfrey]','MiddleName' => '[Germanus]','LastName' => '[Charle]','email' => '[godfreyc033@gmail]','PhoneNUmber' => '[0612066003]','Address' => '[72,Inyala]','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '2','FirstName' => 'Elias ','MiddleName' => 'Mwamba','LastName' => 'shega','email' => 'eliasm123@gmail.com','PhoneNUmber' => '0753210134','Address' => '72,Iwambi','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '3','FirstName' => 'Jafari','MiddleName' => 'Adam','LastName' => 'Seleman','email' => 'jafaria342@gmail.com','PhoneNUmber' => '0612033660','Address' => '72,Magoto','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '4','FirstName' => 'Adam','MiddleName' => 'Lumanga','LastName' => 'Lupyana','email' => 'eadaml009@gmail.com','PhoneNUmber' => '0755339821','Address' => '72,Gedeli','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '5','FirstName' => 'Gelard','MiddleName' => 'Kamuhabwa','LastName' => 'Lutashobya','email' => 'gelardk775@gmail.com','PhoneNUmber' => '0624315678','Address' => '72,Iyunga','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '6','FirstName' => 'Jerome','MiddleName' => 'Joramu','LastName' => 'Mbogo','email' => 'jeromej221@gmail.com','PhoneNUmber' => '0754969303','Address' => '72,Nzega','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '7','FirstName' => 'Benedictor','MiddleName' => 'Willbert','LastName' => 'John','email' => 'benwill456@gmail.com','PhoneNUmber' => '0755239809','Address' => '72,Maswa','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '8','FirstName' => 'Leonard','MiddleName' => 'Papaa','LastName' => 'Ndaki','email' => 'leonardp417@gmail.com','PhoneNUmber' => '0612009988','Address' => '72,Nindo','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '9','FirstName' => 'Marwa','MiddleName' => 'Chacha','LastName' => 'Juma','email' => 'marwac002@gmail.com','PhoneNUmber' => '0748491440','Address' => '72,Ikuti','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '10','FirstName' => 'Joshua','MiddleName' => 'Machilu','LastName' => 'Fanani','email' => 'joshuam006@gmail.com','PhoneNUmber' => '0756227764','Address' => '72,Ghana','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '11','FirstName' => 'Kulwa','MiddleName' => 'Manala','LastName' => 'Ashelly','email' => 'kulwamanala@gmail.com','PhoneNUmber' => '0753293894','Address' => '72,Sido','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '12','FirstName' => 'Elias ','MiddleName' => 'Mwamba','LastName' => 'shega','email' => 'eliasm123@gmail.com','PhoneNUmber' => '0753210134','Address' => '72,Iwambi','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '13','FirstName' => 'Jafari','MiddleName' => 'Adam','LastName' => 'Seleman','email' => 'jafaria342@gmail.com','PhoneNUmber' => '0612033660','Address' => '72,Magoto','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '14','FirstName' => 'Adam','MiddleName' => 'Lumanga','LastName' => 'Lupyana','email' => 'eadaml009@gmail.com','PhoneNUmber' => '0755339821','Address' => '72,Gedeli','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '15','FirstName' => 'Gelard','MiddleName' => 'Kamuhabwa','LastName' => 'Lutashobya','email' => 'gelardk775@gmail.com','PhoneNUmber' => '0624315678','Address' => '72,Iyunga','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '16','FirstName' => 'Jerome','MiddleName' => 'Joramu','LastName' => 'Mbogo','email' => 'jeromej221@gmail.com','PhoneNUmber' => '0754969303','Address' => '72,Nzega','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '17','FirstName' => 'Benedictor','MiddleName' => 'Willbert','LastName' => 'John','email' => 'benwill456@gmail.com','PhoneNUmber' => '0755239809','Address' => '72,Maswa','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '18','FirstName' => 'Leonard','MiddleName' => 'Papaa','LastName' => 'Ndaki','email' => 'leonardp417@gmail.com','PhoneNUmber' => '0612009988','Address' => '72,Nindo','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '19','FirstName' => 'Marwa','MiddleName' => 'Chacha','LastName' => 'Juma','email' => 'marwac002@gmail.com','PhoneNUmber' => '0748491440','Address' => '72,Ikuti','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '20','FirstName' => 'Joshua','MiddleName' => 'Machilu','LastName' => 'Fanani','email' => 'joshuam006@gmail.com','PhoneNUmber' => '0756227764','Address' => '72,Ghana','Date' => '0000-00-00 00:00:00.000000'),
+  array('CustomerID' => '21','FirstName' => 'Kulwa','MiddleName' => 'Manala','LastName' => 'Ashelly','email' => 'kulwamanala@gmail.com','PhoneNUmber' => '0753293894','Address' => '72,Sido','Date' => '0000-00-00 00:00:00.000000')
+);
+
+/* `restaurant`.`dishes` */
+$dishes = array(
+  array('DishID' => '1','DishName' => 'Wali','Description' => 'Wali maharage mchuzi wa nyama ','CustomerID' => '4'),
+  array('DishID' => '2','DishName' => 'ugali','Description' => 'ugali dagaa chainiz','CustomerID' => '1'),
+  array('DishID' => '3','DishName' => 'Tambi','Description' => 'tambi maini mayai','CustomerID' => '9'),
+  array('DishID' => '4','DishName' => 'Matoke','Description' => 'matoke ndizi nyama','CustomerID' => '5'),
+  array('DishID' => '5','DishName' => 'Mtori','Description' => 'mtori nyama ','CustomerID' => '8'),
+  array('DishID' => '6','DishName' => 'Chips','Description' => 'viaz nyama ','CustomerID' => '7'),
+  array('DishID' => '7','DishName' => 'Supu','Description' => 'supu nyama pilipili','CustomerID' => '10'),
+  array('DishID' => '8','DishName' => 'Mapanki','Description' => 'Mchuzi wa samaki pilipili','CustomerID' => '11'),
+  array('DishID' => '9','DishName' => 'Clips','Description' => 'viaz mkaango','CustomerID' => '13'),
+  array('DishID' => '10','DishName' => 'Nyama','Description' => 'nyama choma pilipili','CustomerID' => '12'),
+  array('DishID' => '12','DishName' => 'Wali','Description' => 'Wali maharage mchuzi wa nyama ','CustomerID' => '14'),
+  array('DishID' => '13','DishName' => 'ugali','Description' => 'ugali dagaa chainiz','CustomerID' => '19'),
+  array('DishID' => '14','DishName' => 'Tambi','Description' => 'tambi maini mayai','CustomerID' => '18'),
+  array('DishID' => '15','DishName' => 'Matoke','Description' => 'matoke ndizi nyama','CustomerID' => '16'),
+  array('DishID' => '16','DishName' => 'Mtori','Description' => 'mtori nyama ','CustomerID' => '15'),
+  array('DishID' => '17','DishName' => 'Kisahani1','Description' => 'viaz nyama ','CustomerID' => '14'),
+  array('DishID' => '18','DishName' => 'Makangi','Description' => 'Samaki wa kuchomwa pilipili','CustomerID' => '13'),
+  array('DishID' => '19','DishName' => 'Samakimkaa','Description' => 'Samaki mkaango pilipili','CustomerID' => '11'),
+  array('DishID' => '20','DishName' => 'Mihogo','Description' => 'Mihogo mkaango','CustomerID' => '8'),
+  array('DishID' => '21','DishName' => 'Mshikaki','Description' => 'nyama choma pilipili','CustomerID' => '5')
+);
+
+/* `restaurant`.`orders` */
+$orders = array(
+  array('OrderID' => '1','Price' => '999.99','DishID' => '13','CustomerID' => '11'),
+  array('OrderID' => '2','Price' => '999.99','DishID' => '19','CustomerID' => '19'),
+  array('OrderID' => '3','Price' => '999.99','DishID' => '4','CustomerID' => '13'),
+  array('OrderID' => '4','Price' => '999.99','DishID' => '15','CustomerID' => '17'),
+  array('OrderID' => '5','Price' => '999.99','DishID' => '6','CustomerID' => '17'),
+  array('OrderID' => '6','Price' => '999.99','DishID' => '9','CustomerID' => '15'),
+  array('OrderID' => '7','Price' => '999.99','DishID' => '7','CustomerID' => '17'),
+  array('OrderID' => '8','Price' => '999.99','DishID' => '14','CustomerID' => '9'),
+  array('OrderID' => '9','Price' => '999.99','DishID' => '1','CustomerID' => '13'),
+  array('OrderID' => '10','Price' => '999.99','DishID' => '2','CustomerID' => '6'),
+  array('OrderID' => '12','Price' => '500.00','DishID' => '13','CustomerID' => '11'),
+  array('OrderID' => '13','Price' => '230.00','DishID' => '19','CustomerID' => '19'),
+  array('OrderID' => '14','Price' => '620.00','DishID' => '4','CustomerID' => '13'),
+  array('OrderID' => '15','Price' => '960.00','DishID' => '15','CustomerID' => '17'),
+  array('OrderID' => '16','Price' => '300.00','DishID' => '6','CustomerID' => '17'),
+  array('OrderID' => '17','Price' => '670.00','DishID' => '9','CustomerID' => '15'),
+  array('OrderID' => '18','Price' => '350.00','DishID' => '7','CustomerID' => '17'),
+  array('OrderID' => '19','Price' => '980.00','DishID' => '14','CustomerID' => '9'),
+  array('OrderID' => '20','Price' => '550.00','DishID' => '1','CustomerID' => '13'),
+  array('OrderID' => '21','Price' => '450.00','DishID' => '2','CustomerID' => '6')
+);
